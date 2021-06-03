6381ede179b150092ae6493e5ec2f48b1f3369f5
elan-sk <elan-sk@hotmail.com>
2021-06-02 19:24:06 -0500

    variable numero

---
+++ javascript/Clase2 String.js
@@ -0,0 +1,21 @@
+var texto = 'Elan Francisco Perea Asprilla';
+
+//Convertir texto a Mayusculas
+var textoEnMayusculas = texto.toUpperCase();
+
+//Convertir texto a Minusculas
+var textoEnMinusculas = texto.toLowerCase();
+
+//obtener una letra del texto
+var letra = texto.charAt(0);
+
+//Cantidad de letra del texto
+var cantidadDeLetras = texto.length;
+ 
+//concatenar texto o puedes usar +
+var apodo = 'sk' ;
+var textoNuevo = `${texto} - ${apodo.toUpperCase()}`;
+
+//Parte del texto
+var parteTexto = texto.substr(5,5);
+

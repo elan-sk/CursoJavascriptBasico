d3ed2d2ce0902def91cd915463a231b440e52e77
elan-sk <elan-sk@hotmail.com>
2021-07-07 20:34:40 -0500

    clase 5 objetos

--- javascript/Clase4 funciones.js
+++ javascript/Clase4 funciones.js
@@ -1,15 +1,7 @@
 var nombre = 'elan', edad = 28;
 
-<<<<<<< HEAD
-function imprimirEdad(n,e) {
-    console.log(`${n} tiene ${e} años`);
-}
-
-imprimirEdad('Marcela',26);
-=======
 function imprimirEdad() {
     console.log(`${nombre} tiene ${edad} años`);
 }
 
 imprimirEdad();
->>>>>>> 1e399d161d09df5cd0c9f0adfe5623ef2d0340cc
---
+++ javascript/clase5 objetos.js
@@ -0,0 +1,11 @@
+var elan = {
+    nombre: 'Elan francisco',
+    apellidos:'Perea Aprilla',
+    edad:0
+}
+
+function imprimirNombreEnMayusculas({ nombre }){
+    console.log(nombre.toUpperCase());
+}
+
+imprimirNombreEnMayusculas(elan);
